7fecafa738ddb5137f5a8fadd7802db14adb2e36
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-09-30 13:47:10 +0200

    Delete scope directories

--- predict-dot-loan/contracts/interfaces/IUmaCtfAdapter.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
-
-interface IUmaCtfAdapter {
-    function getExpectedPayouts(bytes32 questionId) external view returns (uint256[] memory);
-}
